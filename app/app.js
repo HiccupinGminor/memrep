33aa64d4e33a1625f20c909d05ab7921e4c206ac
Chris Chung <hiccupinGminor@gmail.com>
2015-02-26 12:22:29 -0800

    Add angular-route, add menu, and navigation partial

--- app/app.js
+++ app/app.js
@@ -1,1 +1,20 @@
-var app = angular.module('app', ['LocalStorageModule']);
+var app = angular.module('app', ['LocalStorageModule', 'ngRoute']);
+
+app.config(['$routeProvider', function($routeProvider){
+
+	$routeProvider
+		.when('/cards', {
+			templateUrl: 'app/templates/CardListControllerTemplate.html',
+			controller: 'CardListController'
+		})
+		.when('/review', {
+			templateUrl: 'app/templates/QuizControllerTemplate.html',
+			controller: 'QuizController',
+		})
+		.when('/', {
+			templateUrl: 'app/templates/MenuTemplate.html',
+		})
+		.otherwise({
+			redirectTo: '/'
+		});
+}]);
